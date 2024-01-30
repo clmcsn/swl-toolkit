d452f4d91e5a2236ff1c05287e92fc552669d219
Giuseppe Maria Sarda <giuseppe.sarda16@gmail.com>
2024-01-30 22:07:28 +0100

    minor fix

--- inputs/templates/vecadd_airbender.py
+++ inputs/templates/vecadd_airbender.py
@@ -7,12 +7,14 @@ vecaddAirbenderCmd.update({
     "app"               : '--app={app} --perf',
     "workload_size" : '--args="-n {workload_size} ',
     "optimize" : '-O {optimize} ',
+    "repeat" : '-r {repeat} ',
     "kernel" : '-k {kernel}"'
 })
 
 vecaddAirbenderDefaults = dict(VortexDefaults)
 vecaddAirbenderDefaults.update({
     "workload_size" : "256",
+    "repeat" : "1",
     "optimize" : "0",
     "kernel" : "vecadd"
 })
@@ -21,6 +23,7 @@ vecaddAirbenderResFname = dict(VortexResultFname)
 vecaddAirbenderResFname.update({
     "workload_size" : 'n{workload_size}',
     "optimize" : 'O{optimize}',
+    "repeat" : 'r{repeat}',
     "kernel" : '{kernel}',
     "groups" : 'hw{groups}',
 })
